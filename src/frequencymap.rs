6adef17b7699d58f088fae4a9a2faa5b989e77e9
Yassine Zakhama <yzakhama@gmail.com>
2024-11-24 22:10:00 +0100

    implement calculate_frequency_map functionality

--- src/frequencymap.rs
+++ src/frequencymap.rs
@@ -1,7 +1,14 @@
 use std::collections::HashMap;
 
 pub fn calculate_frequency_map(s: &str) -> HashMap<char, i32> {
-    HashMap::new()
+    let mut result = HashMap::new();
+
+    for c in s.chars() {
+        let curr = result.entry(c).or_insert(0);
+        *curr += 1;
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -14,16 +21,17 @@ mod tests {
 
         let result = calculate_frequency_map(INPUT);
 
-        assert_eq!(result.len(), 9);
+        assert_eq!(result.len(), 10);
 
         assert_eq!(result.get(&'H').unwrap(), &1);
         assert_eq!(result.get(&'e').unwrap(), &1);
         assert_eq!(result.get(&'l').unwrap(), &3);
         assert_eq!(result.get(&'o').unwrap(), &2);
+        assert_eq!(result.get(&',').unwrap(), &1);
+        assert_eq!(result.get(&' ').unwrap(), &1);
         assert_eq!(result.get(&'w').unwrap(), &1);
         assert_eq!(result.get(&'r').unwrap(), &1);
         assert_eq!(result.get(&'d').unwrap(), &1);
-        assert_eq!(result.get(&',').unwrap(), &1);
         assert_eq!(result.get(&'!').unwrap(), &1);
     }
 }
